9d48e4f91173ac95db19b21116989207edaf17f3
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-14 21:39:38 -0300

    adcionando templates/listando aventos

--- core/views.py
+++ core/views.py
@@ -1,1 +1,11 @@
+from django.http import response
 from django.shortcuts import render, HttpResponse
+from core.models import Evento
+
+
+def lista_eventos(request):
+    # usario = request.user
+    # evento = Evento.objects.filter(usuario=usario)
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
