0cb1b3cf3f3f58068b10c72918b4afc342f892db
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-15 12:04:33 -0300

    adcionando login/logout + arquivos staticos

--- core/views.py
+++ core/views.py
@@ -1,11 +1,36 @@
 from django.http import response
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválida")
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    # usario = request.user
-    # evento = Evento.objects.filter(usuario=usario)
-    evento = Evento.objects.all()
+    user = request.user
+    evento = Evento.objects.filter(usuario=user)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
